2c7b360eb504a5c629d78db4b2ba02305e4c5a8b
Paul <m4rtinpf@gmail.com>
2021-01-28 17:39:15 -0300

    Post update

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,14 +19,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/m4rtinpf'),
+          ('LinkedIn', 'https://www.linkedin.com/in/m4rtinpf'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -39,3 +39,5 @@ from pelican_jupyter import markup as nb_markup
 PLUGINS = [nb_markup]
 
 IGNORE_FILES = [".ipynb_checkpoints"]
+
+THEME = 'pelican-themes/bootstrap'
